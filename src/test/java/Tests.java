793575821a630c87019d6c067d13a2a885da411f
justcasualguy <kubachw@gmail.com>
2020-12-06 22:30:26 +0100

    Init

---
+++ src/test/java/Tests.java
@@ -0,0 +1,24 @@
+package test.java;
+
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    @Test
+    public void test1(){
+        System.out.println("===============");
+        System.out.println("Test 1 SUCCESS");
+    }
+    @Test
+    public void test2(){
+        System.out.println("===============");
+        System.out.println("Test 2 SUCCESS");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("===============");
+        System.out.println("Test 3 SUCCESS");
+    }
+
+}
